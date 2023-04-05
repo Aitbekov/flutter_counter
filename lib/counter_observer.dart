46199ef5d74ea00605d54217fd7d5eb23daf3df9
Bakdaulet <90865998+Aitbekov@users.noreply.github.com>
2023-04-05 12:19:37 +0600

    first commit

---
+++ lib/counter_observer.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+
+class CounterObserver extends BlocObserver {
+  CounterObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+}
+
+
+
+
+
+
+
+
